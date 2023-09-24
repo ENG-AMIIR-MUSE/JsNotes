9807749d1c4d7d7f3b901d4e721ab428c02ed9b0
AmirMuse <amirlearn08@gmail.com>
2023-09-23 21:53:08 -0700

    first day notes completed

---
+++ day 01/variables.js
@@ -0,0 +1,60 @@
+/*
+Variables
+ Most of the time, a JavaScript application needs to work with information. Here are two examples:
+
+ An online shop – the information might include goods being sold and a shopping cart.
+ A chat application – the information might include users, messages, and much more.
+ Variables are used to store this information.
+
+ variables are the reference which points to memory location 
+
+A variable is a “named storage” for data. We can use variables to store goodies, visitors, and other data.
+
+To create a variable in JavaScript, use the let keyword.
+
+The statement below creates (in other words: declares) a variable with the name “message”:
+
+
+Program : set of instructions that performs some actions 
+
+Process: is the running state of the program or excuting state 
+why we require variables 
+-----------------------------
+so if your program is in the running state , at that time to 
+perfom action you need some data   
+and to store data in the memory while excuting the program 
+, so we can utilize  that data to make some actions 
+ you need variables to store that data 
+
+
+or some times you generate a data after excuting Program , and ou will need to store  that data 
+
+how to create a variable 
+to create a variable you have to use it  scope statements  the variable name the   assgiment operator  then the value 
+
+scope statement : 
+const  , let , var  
+
+variable syntax : 
+const  variableName  = value 
+
+variable declarion :  means just creating  a variable  , inside the ram some box will be created  and the box will have some some address 
+
+
+*/
+// variable declarations 
+let name ; 
+let age ;
+
+// storing data to the variables / variable assigment
+name   = "amir"
+age  = 29 ; 
+
+// variable intialization : declating  the variable and storing data at the same time 
+let city  =  "mogadhisho"
+console.log(`my city is ${city}`)
+
+
+console.log(`Hello My name is ${name} and my age is ${age}`)
+
+
