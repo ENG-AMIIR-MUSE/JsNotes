e2c0a38ab76c4d3fab4dde973edae6a2a965215d
AmirMuse <amirlearn08@gmail.com>
2023-10-26 07:30:08 -0700

    day 6 , working with dates

---
+++ day 6 exercise/index.js
@@ -0,0 +1,14 @@
+let date  = new Date()
+// date  = new Date("september 14 2015 1:13:00")
+// console.log(date.getDate())
+date.getDate() // get date as number 
+console.log(date.getFullYear()) // year as a number 
+console.log(date.getMonth()) // month as a number 
+console.log(date.getDate()) // day as a number 
+console.log(date.getHours()) // hours as a number 
+console.log(date.getMinutes()) // Minutes as a number 
+console.log(date.getSeconds()) // seconds  as a number 
+
+const d  = new Date()
+console.log(d.getMonth())
+
