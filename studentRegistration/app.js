9f080e2ed70165608742dc1c7e8068664bd64d5f
AmirMuse <amirlearn08@gmail.com>
2023-11-24 08:30:23 -0800

    added and reomve

--- studentRegistration/app.js
+++ studentRegistration/app.js
@@ -4,6 +4,7 @@ let form = document.querySelector(".courseForm"),
     reigster = document.querySelector("#register");
     message = document.querySelector(".message");
     add = document.querySelector(".add");
+    container = document.querySelector('.container')
     courses  = JSON.parse(localStorage.getItem("courses") || "[]")
 
 add.addEventListener("click", () => {
@@ -15,10 +16,68 @@ reigster.addEventListener("click", (e) => {
   if (courseName.value == "" || code.value == "") {
     alert("all fields are requried");
   }else{
-   courses.push({courseName:courseName.value, code:code.value})
-   localStorage.setItem("courses",JSON.stringify(courses))
-   console.log(courses)
+    addCourse(courseName, code)
+    courseName.value = ""
+    code.value = ""
+    form.classList.toggle("show");
 
   }
 });
+
+display(courses)
+function display(allCourses){
+    let output  = ``
+
+    output += `
+    <table>
+    <tr>
+    <th>course Name</th>
+    <th>course Code</th>
+    <th>Action</th>
+    <tr>
+
+    `
+    if(allCourses.length > 0){
+console.log("greater then ")
+    allCourses.forEach((course,index)=>{
+        output += `
+        <tr>
+            <td>
+            ${course.courseName}
+            </td>
+            <td>
+            ${course.code}
+            </td>
+            <td>
+            <button onclick="editCourse(${index})">Edit</button>
+            <button onclick="removeCourse(${index})">remove</button>
+            </td>
+          
+        </tr>
+        `
+
+    })
+    output += '</table>'
+    console.log(output)
+    container.innerHTML = output
+}else{
+    console.log('less then ')
+    output += `<tr><td>No courses added yet</td></tr>`
+    container.innerHTML =  output
+
+}
+  
+
+}
+function addCourse(courseName,code){
+    courses.push({courseName:courseName.value, code:code.value})
+    localStorage.setItem("courses",JSON.stringify(courses))
+    display(courses)
+}
+
+function removeCourse(index){
+    courses.splice(index,1)
+    localStorage.setItem("courses",JSON.stringify(courses)) 
+    display(courses)
+}
 console.log(courses)
